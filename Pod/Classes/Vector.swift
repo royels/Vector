4e8194c64d38799dd9cf801053bee3349c42aa28
royels <docmentz@gmail.com>
2016-02-23 14:00:00 +0100

    Maybe this should be in a README

---
+++ Pod/Classes/Vector.swift
@@ -0,0 +1,24 @@
+
+
+class Vector {
+    
+/*
+
+    TODO:
+    Double integrals in rectangular
+    Triple integrlas
+    Change of vars
+    Surface Integrals
+    Curl 
+    Divergenc
+    Green and Stokes
+    Vector Field
+    
+    
+    
+    
+    
+    
+*/ 
+    
+}
